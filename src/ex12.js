c4e8718dcdc74b2c9dc16af426750f1d7d6dd6e1
JGamito <gamito90@hotmail.com>
2022-04-19 18:23:25 +0200

    All exercices Finished

--- src/ex12.js
+++ src/ex12.js
@@ -1,16 +1,14 @@
 const person = {
-    name: 'Jaume',
-    surname: 'Meskini', 
-    age: 31
+  name: "Jaume",
+  surname: "Meskini",
+  age: 31,
 };
 
-/*console.log('My full name is: ' +  + ' ' + );
-console.log('My age is: ' + );*/
+console.log("My full name is: " + person.name + " " + person.surname);
+console.log("My age is: " + person.age);
 
 /*
 output:
-
 My full name is: Jaume Meskini
 My age is: 31
-
 */
